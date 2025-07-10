37b87de082e263aa10697cf85da192919b9b2e6f
Teja-00-cp <KonathalaTeja.Satya@cognizant.com>
2025-07-10 17:31:05 +0530

    serviceImpl

--- src/main/java/com/pod2/Ecommerce/ServicesImpl/orderServiceImpl.java
+++ src/main/java/com/pod2/Ecommerce/ServicesImpl/orderServiceImpl.java
@@ -1,3 +1,4 @@
+package com.pod2.Ecommerce.ServicesImpl;
 // package ServicesImpl;
 
 // public class orderServiceImpl 
--- src/main/java/com/pod2/Ecommerce/ServicesImpl/userServiceImpl.java
+++ src/main/java/com/pod2/Ecommerce/ServicesImpl/userServiceImpl.java
@@ -1,3 +1,4 @@
+package com.pod2.Ecommerce.ServicesImpl;
 // package com.pod2.Ecommerce.ServicesImpl;
 
 // public class userServiceImpl 
